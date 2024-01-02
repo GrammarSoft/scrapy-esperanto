ca973db3e5c2403553e01b04c8c3a379aed07ba3
Tino Didriksen <mail@tinodidriksen.com>
2024-01-02 16:25:06 +0000

    WIP

--- esperanto/spiders/gspider.py
+++ esperanto/spiders/gspider.py
@@ -1,19 +1,67 @@
+import os
+import subprocess
+import sqlite3
+import regex as re
+import hashlib
 from pathlib import Path
+from scrapy.linkextractors import LinkExtractor
 import scrapy
 
 class GSpider(scrapy.Spider):
-	name = "quotes"
+	name = "gspider"
+
+	link_extractor = None
+	con = None
+	db = None
+
+	def __init__(self, category=None, *args, **kwargs):
+		super(GSpider, self).__init__(*args, **kwargs)
+
+		dir = os.path.dirname(__file__)
+		os.chdir(dir)
+
+		self.link_extractor = LinkExtractor(deny_domains=['wikitrans.net', 'epo.wikitrans.net', 'dan.wikitrans.net'], unique=True)
+
+		subprocess.run(['sqlite3', 'spider.sqlite', '-init', 'schema.sql'], input='')
+		self.con = sqlite3.connect('spider.sqlite')
+		self.db = self.con.cursor()
 
 	def start_requests(self):
-		urls = [
-			"https://quotes.toscrape.com/page/1/",
-			"https://quotes.toscrape.com/page/2/",
-		]
+		self.db.execute("SELECT q_url FROM sc_queue ORDER BY q_stamp ASC, q_url ASC")
+		urls = self.db.fetchall()
+
+		if not urls:
+			urls = Path('urls.txt').read_text(encoding='UTF-8')
+			urls = re.sub(r'#[^\n]+\n', r'', urls)
+			urls = urls.strip().split('\n')
+			for url in urls:
+				self.db.execute("INSERT INTO sc_queue (q_url) VALUES (?)", [url])
+			self.con.commit()
+			self.db.execute("SELECT q_url FROM sc_queue ORDER BY q_stamp ASC, q_url ASC")
+			urls = self.db.fetchall()
+
 		for url in urls:
-			yield scrapy.Request(url=url, callback=self.parse)
+			self.log(f'Queued {url[0]}')
+			yield scrapy.Request(url=url[0], callback=self.parse)
 
 	def parse(self, response):
-		page = response.url.split("/")[-2]
-		filename = f"quotes-{page}.html"
-		Path(filename).write_bytes(response.body)
-		self.log(f"Saved file {filename}")
+		body = response.text
+
+		h = hashlib.sha1(response.body).hexdigest()
+		self.db.execute("SELECT r_url FROM sc_results WHERE r_hash = ?", [h])
+		rows = self.db.fetchall()
+		if rows:
+			self.log(f'Seen {response.url}')
+			# We've already seen this result body
+			return None
+
+		headers = ''
+		for k,v in response.headers.items():
+			headers += f'{k}: {v}\n'
+
+		self.db.execute("INSERT INTO sc_results (r_hash, r_url, r_headers, r_body) VALUES (?, ?, ?, ?)", [h, response.url, headers, body])
+
+		self.db.execute("DELETE FROM sc_queue WHERE q_url = ?", [response.url])
+		self.con.commit()
+		self.log(f'Stored {response.url}')
+		return None
---
+++ esperanto/spiders/schema.sql
@@ -0,0 +1,29 @@
+PRAGMA case_sensitive_like = ON;
+PRAGMA foreign_keys = OFF;
+PRAGMA journal_mode = MEMORY;
+PRAGMA locking_mode = EXCLUSIVE;
+PRAGMA synchronous = OFF;
+PRAGMA threads = 4;
+PRAGMA trusted_schema = OFF;
+PRAGMA page_size = 65535;
+VACUUM;
+PRAGMA locking_mode = NORMAL;
+
+
+CREATE TABLE IF NOT EXISTS sc_queue (
+	q_url TEXT NOT NULL,
+	q_stamp DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+	PRIMARY KEY (q_url)
+);
+
+
+CREATE TABLE IF NOT EXISTS sc_results (
+	r_hash TEXT NOT NULL,
+	r_url TEXT NOT NULL,
+	r_headers TEXT NOT NULL,
+	r_body TEXT NOT NULL,
+	r_stamp DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+	PRIMARY KEY (r_hash)
+);
