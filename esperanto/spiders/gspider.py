496ea9ab916764e9d0c09b1149db18367fd05b3c
Tino Didriksen <mail@tinodidriksen.com>
2024-01-03 15:34:30 +0000

    WIP

--- esperanto/spiders/gspider.py
+++ esperanto/spiders/gspider.py
@@ -20,10 +20,10 @@ class GSpider(scrapy.Spider):
 		dir = os.path.dirname(__file__)
 		os.chdir(dir)
 
-		self.link_extractor = LinkExtractor(deny_domains=['wikitrans.net', 'epo.wikitrans.net', 'dan.wikitrans.net'], unique=True)
+		self.link_extractor = LinkExtractor(deny=[r'\.wiki(news|pedia|books)\.org'], deny_domains=['wikitrans.net', 'epo.wikitrans.net', 'dan.wikitrans.net', 'eo.wikinews.org', 'eo.wikipedia.org', 'eo.wikibooks.org'], unique=True)
 
-		subprocess.run(['sqlite3', 'spider.sqlite', '-init', 'schema.sql'], input='')
-		self.con = sqlite3.connect('spider.sqlite')
+		subprocess.run(['sqlite3', 'esperanto.sqlite', '-init', 'schema.sql'], input='')
+		self.con = sqlite3.connect('esperanto.sqlite')
 		self.db = self.con.cursor()
 
 	def start_requests(self):
@@ -41,17 +41,26 @@ class GSpider(scrapy.Spider):
 			urls = self.db.fetchall()
 
 		for url in urls:
-			self.log(f'Queued {url[0]}')
+			self.log(f'Queued start {url[0]}')
 			yield scrapy.Request(url=url[0], callback=self.parse)
 
 	def parse(self, response):
 		body = response.text
 
+		good = False
+		if re.search(r' lang=["\']?eo', body) or re.search(r'[ĈĉĜĝĴĵŜŝŬŭ]', body) or re.search(r'&#(264|265|284|285|308|309|348|349|364|365);', body) or re.search(r'&#[xX](108|109|11C|11D|134|135|15C|15D|16C|16D);', body):
+			good = True
+
+		if not good:
+			self.log(f'Not Esperanto {response.url}')
+			self.db.execute("INSERT INTO sc_queue_not (q_url) VALUES (?)", [response.url])
+			self.con.commit()
+			return None
+
 		h = hashlib.sha1(response.body).hexdigest()
 		self.db.execute("SELECT r_url FROM sc_results WHERE r_hash = ?", [h])
-		rows = self.db.fetchall()
-		if rows:
-			self.log(f'Seen {response.url}')
+		if self.db.fetchall():
+			self.log(f'Seen parse {response.url}')
 			# We've already seen this result body
 			return None
 
@@ -61,7 +70,34 @@ class GSpider(scrapy.Spider):
 
 		self.db.execute("INSERT INTO sc_results (r_hash, r_url, r_headers, r_body) VALUES (?, ?, ?, ?)", [h, response.url, headers, body])
 
+		urls = []
+		links = self.link_extractor.extract_links(response)
+		for link in links:
+			if link.nofollow:
+				self.log(f'Nofollow {response.url} => {link.url}')
+				continue
+			link.url = re.sub(r'#.*$', r'', link.url)
+
+			self.db.execute("SELECT r_url FROM sc_results WHERE r_url = ?", [link.url])
+			if self.db.fetchall():
+				self.log(f'Seen result {response.url} => {link.url}')
+				continue
+			self.db.execute("SELECT q_url FROM sc_queue WHERE q_url = ?", [link.url])
+			if self.db.fetchall():
+				self.log(f'Seen queue {response.url} => {link.url}')
+				continue
+			self.db.execute("SELECT q_url FROM sc_queue_not WHERE q_url = ?", [link.url])
+			if self.db.fetchall():
+				self.log(f'Seen queue_not {response.url} => {link.url}')
+				continue
+
+			self.db.execute("INSERT OR IGNORE INTO sc_queue (q_url) VALUES (?)", [link.url])
+			urls.append(link.url)
+
 		self.db.execute("DELETE FROM sc_queue WHERE q_url = ?", [response.url])
 		self.con.commit()
 		self.log(f'Stored {response.url}')
-		return None
+
+		for url in urls:
+			self.log(f'Queued parse {response.url} => {url}')
+			yield scrapy.Request(url=url, callback=self.parse)
